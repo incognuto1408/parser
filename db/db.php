6dcfc45f0cb2261e240a142dbabd71f97afb0a51
incognuto1408 <incognuto1408@gmail.com>
2020-01-23 03:11:52 +0300

    Удалил бд с лог пас

--- db/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-error_reporting(0);
-$db = new PDO('mysql:host=localhost;dbname=parser_domain;charset=UTF8','parser_domain','03011998n');
-header('Content-Type: text/html; charset=utf-8', true);
-$error = $_SESSION['error'];
-$login = $_SESSION['login'];
-$password = $_SESSION['password'];
